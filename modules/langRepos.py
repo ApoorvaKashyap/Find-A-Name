38ec1004404bba215507cfcb5846ec52e258b482
ApoorvaKashyap <apoorvakashyap54@gmail.com>
2021-07-28 11:22:22 +0530

    Added support for CPPReference

--- modules/langRepos.py
+++ modules/langRepos.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup as bs
 
 
 def pypi(projectName):
@@ -6,7 +7,7 @@ def pypi(projectName):
     endpoint = "/json/"
     url = base + projectName + endpoint
     rawResponse = requests.get(url)
-    if '404' not in str(rawResponse):
+    if "404" not in str(rawResponse):
         # response = rawResponse.json()
         return 0
     else:
@@ -25,6 +26,31 @@ def npm(projectName):
         return 0
 
 
+def rubyGems(projectName):
+    base = "https://rubygems.org/api/v1/search.json?query="
+    url = base + projectName
+    rawResponse = requests.get(url)
+    response = rawResponse.json()
+    if response:
+        return 0
+    else:
+        return 1
+
+
+def cppReference(projectName):
+    projectName = projectName.lower()
+    base = "https://en.cppreference.com/w/cpp/links/libs"
+    rawResponse = requests.get(base)
+    soup = bs(rawResponse.content, "html.parser")
+    libs = []
+    for lib in soup.find_all("a", {"class": "external text"}):
+        libs.append(lib.text.strip().lower())
+    if projectName in libs:
+        return 0
+    else:
+        return 1
+
+
 if __name__ == "__main__":
-    res = npm("gihk")
+    res = cppReference("libVLC")
     print(res)
